5bff485bcbcf0d2f4495b63c9240934e0e851657
Maryam shahvalizadeh <Maryamshahvalizadeh@gmail.com>
2024-04-23 09:20:55 +0330

    create category part.

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,56 @@
+import { useState } from "react";
 import Loader from "../components/Loader";
 import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductsContext";
-import  styles  from "./ProductsPage.module.css";
+import styles from "./ProductsPage.module.css";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
 
 const ProductsPage = () => {
-    const products = useProducts();
-    console.log(products)
-    return ( 
-       <div className={styles.container}>
+  const products = useProducts();
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+  console.log(products);
+  const searchHandler = () => {
+    console.log("clicked")
+  }
+  const categoryHandler = (event) => {
+    const {tagName} = event.target;
+    const category = event.target.innerText.toLowerCase();
+    if(tagName !== "LI") return;
+  }
+  return (
+    <>
+      <div>
+        <input type="text" onChange={(e) => setSearch(e.target.value.toLowerCase().trim())} value={search} placeholder="Search..." />
+        <button onClick={searchHandler}>
+        <ImSearch />
+        </button>
+      </div>
+      <div className={styles.container}>
         <div className={styles.products}>
-            {!products.length && <Loader />}
-            {products.map((p) => <ProductCard key={p.id} product={p} />)}
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <ProductCard key={p.id} product={p} />
+          ))}
         </div>
-        <div>sidebar</div>
-       </div>
-     );
-}
- 
+        <div>
+            <div>
+                <FaListUl />
+                <p>Categories</p>
+            </div>
+            <ul onClick={categoryHandler}>
+                <li>All</li>
+                <li>Electronics</li>
+                <li>Jewelery</li>
+                <li>Men's clothing</li>
+                <li>Women's clothing</li>
+            </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default ProductsPage;
