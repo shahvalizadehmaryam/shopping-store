9d0709631da9a4462ef335209bdfe9ab52a81e02
Maryam shahvalizadeh <Maryamshahvalizadeh@gmail.com>
2024-04-25 10:21:42 +0330

    added some actions to reducer.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -21,13 +21,36 @@ switch (action.type) {
             checkout:false
 
         }
-        // case "REMOVE_ITEM":
-        //    const filteredSelectedItems = state.selectedItems.filter((item) => item.id !== action.payload.id);
-        //    return{
-        //     ...state,
-        //     selectedItems : [...filteredSelectedItems],
-        //     ...sumProducts(filteredSelectedItems)
-        //    }
+        case "REMOVE_ITEM":
+           const filteredSelectedItems = state.selectedItems.filter((item) => item.id !== action.payload.id);
+           return{
+            ...state,
+            selectedItems : [...filteredSelectedItems],
+            ...sumProducts(filteredSelectedItems)
+           }
+           case "INCREASE":
+            const increaseIndex = state.selectedItems.findIndex((item) => item.id === action.payload.id)
+            state.selectedItems[increaseIndex].quantity++;
+            return{
+                ...state,
+                ...sumProducts(state.selectedItems),
+                checkout:false
+            }
+            case "DECREASE":
+                const decreaseIndex = state.selectedItems.findIndex((item) => item.id === action.payload.id)
+                state.selectedItems[decreaseIndex].quantity--;
+                return{
+                    ...state,
+                    ...sumProducts(state.selectedItems),
+                    checkout:false
+                }
+                case "CHECKOUT":
+                    return{
+                        selectedItems:[],
+                        itemsCounter:0,
+                        total:0,
+                        checkout : true
+                    }
 
     default:
      throw new Error("Invalid Action!");
