9f2cccc02a8a3c4ead34c1b8e5ba6f80fa51b602
Maryam shahvalizadeh <Maryamshahvalizadeh@gmail.com>
2024-04-27 10:14:32 +0330

    developed productDetailsPage

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -3,26 +3,31 @@ import api from "../services/config";
 
 const ProductContext = createContext();
 
-const ProductsProvider = ({children}) => {
-    const [products , setProducts] = useState([])
-useEffect(() => {
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
     const fetchProducts = async () => {
-        try {
-            setProducts(await api.get("/products"));
-        } catch (error) {
-            console.log(error.messages)
-        }
-    }
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.messages);
+      }
+    };
     fetchProducts();
-}, [])
+  }, []);
 
-    return ( 
-       <ProductContext.Provider value={products}>
-        {children}
-       </ProductContext.Provider>
-     );
-}
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
 // custom hook
- export const useProducts = () => useContext(ProductContext)
- 
+export const useProducts = () => useContext(ProductContext);
+export const useProductDetails = (id) => {
+  const products = useContext(ProductContext);
+  const result = products.find((item) => item.id === id);
+  return result;
+};
+
 export default ProductsProvider;
--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,7 +1,37 @@
+import { Link, useParams } from "react-router-dom";
+import { useProductDetails } from "../context/ProductsContext";
+import { SiOpenproject } from "react-icons/si";
+import { IoMdPricetag } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+import Loader from "../components/Loader";
+import styles from "./DetailsPage.module.css";
+
 const DetailsPage = () => {
-    return ( 
-        <h3>DetailsPage</h3>
-     );
-}
- 
+    // inja id string hast
+  const { id } = useParams();
+//   inja ham be number id niyaz hast ke + mizarim ta tabdil be addad beshe.
+  const productDetails = useProductDetails(+id);
+   if(!productDetails) return <Loader />
+  return (
+    <div className={styles.container}>
+      <img src={productDetails.image} alt={productDetails.title} />
+      <div className={styles.information}>
+        <h3>{productDetails.title}</h3>
+        <p className={styles.description}>{productDetails.description}</p>
+        <p className={styles.category}>{productDetails.category}</p>
+        <div>
+          <span className={styles.price}>
+            <IoMdPricetag />
+            {productDetails.price} $
+          </span>
+          <Link to="/products">
+            <FaArrowLeft />
+            <span>Back to shop</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default DetailsPage;
